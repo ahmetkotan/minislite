f79aca06c3a816672638c6bf6a670f83b0a044a1
ahmetkotan <ahmtkotan@gmail.com>
2020-04-08 15:50:33 +0300

    added setup.py and configured pypi

---
+++ setup.py
@@ -0,0 +1,23 @@
+# Third Party
+from setuptools import setup
+
+setup(
+    name="minislite",
+    version="0.9",
+    packages=["minislite"],
+    url="https://github.com/ahmetkotan/minislite",
+    license="",
+    author="ahmetkotan",
+    author_email="ahmtkotan@gmail.com",
+    description="Mini SQLite ORM",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: SQL",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Database",
+        "Topic :: Database :: Database Engines/Servers",
+    ],
+)
